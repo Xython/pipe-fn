300b47140167318c5a3ed18472e85f0e0efff928
thautwarm <twshere@outlook.com>
2017-12-31 12:20:58 +0800

    finished

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 with open('./README.rst', encoding='utf-8') as f:
     readme = f.read()
@@ -12,6 +12,7 @@ setup(
     license='MIT',
     author='thautwarm',
     author_email='twshere@outlook.com',
+    include_package_data=True,
     description='functional pipeline operation in Python',
     platforms='any',
     classifiers=[
@@ -19,6 +20,5 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython'],
-    zip_safe=False
+        'Programming Language :: Python :: Implementation :: CPython']
 )
